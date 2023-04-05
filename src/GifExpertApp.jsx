510d37b2da7ffdad998b1625fb3d85504a3221b9
VictorRF <victor_rofh@hotmail.com>
2023-04-05 17:43:59 -0600

    Companente para agregar categoria

--- src/GifExpertApp.jsx
+++ src/GifExpertApp.jsx
@@ -1,9 +1,27 @@
-import React from 'react'
+import React, { useState } from 'react'
+import { AddCategory } from './components/AddCategory';
 
 export const GifExpertApp = () => {
-  return (
-    <>
-        <h1>Gift Expert</h1>
-     </>
-  )
+
+    const [categories, setCategories] = useState([ 'One Piece', 'Goku' ]);     
+
+     const onAddCategory = ( newCategory ) => {
+        setCategories( [ newCategory, ...categories] );
+     }
+
+    return (
+        <>
+            <h1>Gift Expert</h1>
+
+            <AddCategory onNewCategory={ onAddCategory } />
+
+            <ol>
+                {
+                    categories.map( category => {
+                        return <li key={ category }>{ category }</li>
+                    })
+                }
+            </ol>
+        </>
+    )
 }
